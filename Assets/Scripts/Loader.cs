8093bfacb9973d0a178c8ed74ff85aee29865ad2
Gianti Cristian <gianticristian@gmail.com>
2017-08-22 12:40:57 -0300

    Video 14/14: Mobile. Clear the project. Minor fix

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Loader : MonoBehaviour
 {
     public GameObject gameManager;
+    public GameObject soundManager;  
 
     void Awake()
     {
         if (GameManager.instance == null)
             Instantiate(gameManager);
+        if (SoundManager.instance == null)
+            Instantiate(soundManager);
     }
 }

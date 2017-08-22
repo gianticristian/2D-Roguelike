2f377d660c619ba3da2804840a00b07ad69bd72d
Gianti Cristian <gianticristian@gmail.com>
2017-08-22 10:26:45 -0300

    Video 12/14: UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    public float levelStartDelay = 2f;
     public float turnDelay = 0.1f;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] 
     public bool playersTurn = true;
 
-    private int level = 3;
+    private Text levelText;
+    private GameObject levelImage;
+    private bool doingSetup;
+    private int level = 1;
     private List<Enemy> enemies;
     private bool enemiesMoving;
 
@@ -28,20 +33,41 @@ public class GameManager : MonoBehaviour
         InitGame();
     }
 
+    private void OnLevelWasLoaded (int index)
+    {
+        level++;
+        InitGame();
+    }
+
     void InitGame ()
     {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
         enemies.Clear();
         boardScript.SetupScene(level);
     }
 
+    private void HideLevelImage ()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
     public void GameOver ()
     {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
         enabled = false;
     }
 
     void Update()
     {
-        if (playersTurn || enemiesMoving)
+        if (playersTurn || enemiesMoving || doingSetup)
             return;
 
         StartCoroutine(MoveEnemies());
